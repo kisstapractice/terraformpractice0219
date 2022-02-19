188a0129e7f00dbad87ac8dcbbe26e175d70a131
kisstapractice <ogrebeer2@gmail.com>
2022-02-19 14:43:58 +0000

    fixes

--- createInstance.tf
+++ createInstance.tf
@@ -6,11 +6,6 @@ data "aws_ami" "latest-ubuntu" {
   owners = ["099720109477"]
 
   filter {
-    name = "name"
-    values = "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"
-  }
-
-  filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "AWS_ACCESS_KEY" {
     type = string
-    default = "AKIASMSIZOF42P2VUDSZ"
+    default = "AKIATMRFM2ULQ3MBVTNG"
 }
 
 variable "AWS_SECRET_KEY" {}
