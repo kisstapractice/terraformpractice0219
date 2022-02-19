8a93f26203f82772beeeb61d8ead0ed1ffbbd25b
RDISOFTWARE\Administrator <support@rdisoftware.com>
2022-02-19 17:40:28 +0100

    vpc 2nd

--- createInstance.tf
+++ createInstance.tf
@@ -1,21 +1,16 @@
 
-data "aws_availability_zones" "avilable" {}
-
-data "aws_ami" "latest-ubuntu" {
-  most_recent = true
-  owners = ["099720109477"]
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+resource "aws_key_pair" "levelup_key" {
+    key_name = "levelup_key"
+    public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
-
 resource "aws_instance" "MyFirstInstnace" {
-  ami           = data.aws_ami.latest-ubuntu.id
+  ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  availability_zone = data.aws_availability_zones.avilable.names[1]
+  key_name      = aws_key_pair.levelup_key.key_name
+
+  vpc_security_group_ids = [aws_security_group.allow-levelup-ssh.id]
+  subnet_id = aws_subnet.levelupvpc-public-2.id
 
   tags = {
     Name = "custom_instance"
